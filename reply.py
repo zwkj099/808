826015b6df997d9c0989fbb7c01e194c15289a08
艾丽丽 <ailili@zwlbs.com>
2019-08-16 20:48:23 +0800

    init

---
+++ reply.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+#应答脚本
+import time
+import threading
+import thread
+import testlibrary
+import random
+import re
+import datetime
+tp=testlibrary.testlibrary()
+
+"""
+1.持续发送位置，心跳报文
+2.监听接收
+3.收到平台报文后，判断是否需要应答
+4.需要应答时组装应答报文发送
+5.应答结果配置设置
+6.多线程
+"""
+
+def reply(link,res,mobile,id,answer_number):
+    try:
+        reno = "00"
+        """
+        x += 1
+        if x%5==1:
+            reno="01"#失败
+        elif x%5==2:
+            reno="02"#消息有误
+        elif x%5==3:
+            reno="03"#不支持
+        elif x%5==4:
+            time.sleep(3)#超时
+        else:
+            pass
+        print "第%d次应答%s" % (x, reno)
+        """
+        rlist = ["8103", "8106", "8107","8201", "8900", "8001"]
+        if id not in rlist:
+            # Usual(link,mobile, id, answer_number, reno)
+            usual_body = get_usyal_body(id, answer_number, reno)
+            usual_head = tp.data_head(mobile, 1, usual_body, 5)
+            usual_redata = tp.add_all(usual_head + usual_body)
+            tp.send_data(link, usual_redata)
+
+        # 平台下发指令8106，查询指定终端参数
+        elif id == "8106":
+            search_body = get_loadres_body(res)
+            search_head = tp.data_head(mobile, 260, search_body, 5)
+            search_data = tp.add_all(search_head + search_body)
+            tp.send_data(link, search_data)
+            #res = tp.receive_data(link)
+
+        # 平台下发指令8107，查询终端属性
+        elif id == "8107":
+            search2_body = get_0107body()
+            search2_head = tp.data_head(mobile, 263, search2_body, 5)
+            search2_data = tp.add_all(search2_head + search2_body)
+            print search2_data
+            tp.send_data(link, search2_data)
+
+            # 平台下发指令8900,油量标定数组
+        elif id == "8900":
+            # Usual(link,mobile, id, answer_number, reno) # 应答通用应答
+            usual_body = get_usyal_body(id, answer_number, reno)
+            usual_head = tp.data_head(mobile, 1, usual_body, 5)
+            usual_redata = tp.add_all(usual_head + usual_body)
+            tp.send_data(link, usual_redata)
+            # Answer_0900(link, mobile, res[34:36], answer_number, reno)  # 应答0900
+            sensor = res[34:36]
+            usual_body = "F301" + sensor + "03" + answer_number + reno
+            usual_head = tp.data_head(mobile, 2304, usual_body, 5)
+            result = tp.add_all(usual_head + usual_body)
+            tp.send_data(link, result)
+        # 平台下发指令8103，传感器参数设置
+        elif id == "8103":
+            # Usual(link, mobile, id, answer_number, reno)  # 应答通用应答
+            usual_body = get_usyal_body(id, answer_number, reno)
+            usual_head = tp.data_head(mobile, 1, usual_body, 5)
+            usual_redata = tp.add_all(usual_head + usual_body)
+            tp.send_data(link, usual_redata)
+            if tp.to_int(res[28:34]) == 243:  # 获取设置的ID，如果是带F3的ID，则通用应答后需要继续应答0900
+                # Answer_0900(link,mobile, res[34:36], answer_number, reno)
+                sensor = res[34:36]
+                usual_body = "F301" + sensor + "03" + answer_number + reno
+                usual_head = tp.data_head(mobile, 2304, usual_body, 5)
+                result = tp.add_all(usual_head + usual_body)
+                tp.send_data(link, result)
+        # 平台下发指令8201，位置信息查询
+        #elif id == "8201":
+            # 组装0201位置数据，包含油量数据、里程数据
+            # mel += 1
+            # AD += 1
+            # Oil += 1
+            # high += 1
+            # oils = [AD, Oil, high]
+            # gpsbody = tp.position(mobile, 513, 2, 0, alarm, status, jin, wei, high, speed, ti, direction, mel,
+            #                       tp.zd_body(wsid, zds), tp.f3_attach(idlist, oils, wds, sds, yhs, zfs, zzs, gss, lcs),
+            #                       version, answer_number)
+            # tp.send_data(link, gpsbody)
+        # 平台下发指令8202,跟踪
+        # elif id == "8202":
+        #     nu = tp.to_int(res[26:30])  # 获取回传间隔时间
+        #     tim = tp.to_int(res[30:38])  # 获取跟踪有效时长
+        #     print answer_number, nu, tim
+        #     # 应答通用应答
+        #     # Usual(link, mobile, id, answer_number, reno)
+        #     usual_body = get_usyal_body(id, answer_number, reno)
+        #     usual_head = tp.data_head(mobile, 1, usual_body, 5)
+        #     usual_redata = tp.add_all(usual_head + usual_body)
+        #     tp.send_data(link, usual_redata)
+        #     i = 0
+            # while i < tim / nu:
+            #     # 组装0202位置数据，包含油量数据、里程数据
+            #     gpsbody = tp.position(mobile, messageid, 2, 0, alarm, status, jin, wei, high, speed, ti, direction, mel,
+            #                           tp.zd_body(wsid, zds),
+            #                           tp.f3_attach(idlist, oils, wds, sds, yhs, zfs, zzs, gss, lcs),
+            #                           version, answer_number)
+            #     tp.send_data(link, gpsbody)
+            #     time.sleep(nu)
+            #     i += 1
+            #     print "第%d次发送跟踪信息：" % i
+
+    except:
+        pass
+#组装响应报文body
+def get_usyal_body(id,ac,reno="00"):
+    body=ac+id+reno
+    return body
+
+#组装读取指令应答body
+def get_loadres_body(data):
+    #取得参数总数（可用于校验，未实现）
+    total=data[26:28]
+    #取得下发报文的流水号
+    ac=data[22:26]
+    #string类型id添加在这里
+    list1=["00000083","00000049","00000048","00000040","00000041","00000042","00000043",
+           "00000044","0000001D","0000001A","00000010","00000011","00000012","00000013",
+           "00000014","00000015","00000016","00000017"]
+    #byte类型id添加在这里
+    list2=["00000084","00000090","00000091","00000092","00000094"]
+    #word类型id添加到这里
+    list3=["00000101","00000103","00000081","00000082","0000005B","0000005C","0000005D",
+           "0000005E","00000031"]
+    #byte[8]类型添加到这里
+    list4=["00000110"]
+    #DWORD类型放在else里面
+    body=data[28:-4]
+    print "get_loadres_body(data)", body
+    parbody=""
+    x=0
+    while body:
+        id=body[0:8]
+        print id
+        body=body[8:]
+        #此处将id和值组装起来，可以添加if条件来特殊处理某些参数，也可以修改一些默认参数、或去掉一些应答
+        #下面组装为id+长度+值
+        if id =="0000F901" or id =="0000F902" or id =="0000F903":
+            da=id+"1904"+"0001050101010005"+"010106020202FFFF"+"020207020303FFFF"+"03020702030300FF"
+        elif id == "0000F904":
+            da=id+"0401020401"
+        elif id == "0000F905":
+            da=id+"0A01000201030204030600"
+        elif id == "0000F906":
+            da=id+"070A000102030000000000000A00010203000000000000"
+        elif id == "0000F641":
+            da=id+"150000003A000000010000007500000005000000B00000000A000000EA0000001000000124000000160000015F0000001D0000019A00000023000001D40000002B0000020E00000032000002490000003A0000028300000042000002BE0000004A000002F80000005100000333000000580000036E00000060000003A800000066000003E30000006B0000041D0000007000000458000000750000049200000078FFFFFFFFFFFFFFFF247E"
+        elif id in list1:
+            da=id+"0474657374"
+        elif id in list2:
+            da=id+"0101"
+        elif id in list3:
+            da=id+"020101"
+        elif id in list4:
+            da=id+"080101010101010101"
+        else:
+            da=id+"0400000001"
+        x+=1
+        parbody=parbody+da
+    resbody=ac+tp.to_hex(x,2)+parbody
+    return resbody
+
+
+#组装传感器拓展查询基本信息应答报文body
+def get_searchsenior_body(data):
+    ac=data[22:26]
+    sid=data[-6:-4]
+    print "this is sid",sid
+    """
+    数据长度+公司名称长度+名称+产品代码长度+代码+硬件版本号长度+硬件版本号+
+    软件版本号长度+版本号+设备id长度+设备id+客户代码长度+客户代码
+    """
+    d=ac+"01"+"0000F8"+sid
+    s="13"+"01"+"31"+"01"+"31"+"01"+"31"+\
+      "01"+"31"+"08"+"3132333435363131"+"01"+"35"
+    body=d+s
+    return body
+#组装0107body
+def get_0107body():
+    """
+    z终端类型（word）+制造商id（b5）+终端型号（b20）+终端id（b7）+sim卡iddid（bcd10）
+    +终端硬件版本长度（b）+硬件版本号+终端固件版本长度（b）+固件版本号+GNSS模块（b）
+    +通信模块（b）
+    """
+    parm="0008"+"3131323334"+"3131323334313132333431313233343131323334"+\
+         "31313233343639"+"31313233343131323334"+"08312E382E352E3132"+\
+         "08312E382E352E3132"+"08"+"08"
+    return parm
+#通用应答
+# def Usual(link,mobile,id,answer_number,reno):
+#     '''组装通用应答报文，并发送
+#            :param link: 连接
+#            :param mobile: 手机号
+#            :param id: 应答消息ID
+#            :param answer_number: 应答流水号
+#            :param reno:应答结果
+#            '''
+#     usual_body = get_usyal_body(id, answer_number, reno)
+#     usual_head = tp.data_head(mobile, 1, usual_body, 5)
+#     result = tp.add_all(usual_head + usual_body)
+#     tp.send_data(link, result)
+
+#0900应答
+# def Answer_0900(link,mobile,sensor,answer_number,reno):
+#     '''组装0900报文，并发送
+#             :param link: 连接
+#             :param mobile: 手机号
+#             :param sensor: 传感器ID
+#             :param answer_number: 应答流水号
+#             :param reno:应答结果
+#             '''
+#     usual_body = "F301"+ sensor +"03" + answer_number + reno
+#     usual_head = tp.data_head(mobile, 2304, usual_body, 5)
+#     result = tp.add_all(usual_head + usual_body)
+#     tp.send_data(link, result)
---
+++ res_ui.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+import time
+import testlibrary
+import threading
+tp=testlibrary.testlibrary()
+from PyQt4 import QtGui, QtCore, uic
+import sys
+#读取ui文件
+
+#wind, base = uic.loadUiType('C:Python27/Lib/site-packages/PyQt4/test.ui')
+wind, base = uic.loadUiType('res1.ui')
+class fwindow(wind, base):
+    #窗口显示
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
+        #self.setWindowIcon(QtGui.QIcon(ico))
+        self.set()
+    def set(self):
+        self.pushButton.clicked.connect(self.start)
+        self.pushButton_2.clicked.connect(self.help)
+        self.show()
+    def start1(self):
+        reno=self.comboBox.currentIndex()
+        print reno
+    def start(self):
+        self.ip=unicode(self.lineEdit.text()).encode("utf-8")
+        self.mobile=unicode(self.lineEdit_2.text()).encode("utf-8")
+        self.number=unicode(self.lineEdit_3.text()).encode("utf-8")
+        mobile=int(self.mobile)
+        number=int(self.number)
+        endmobile=mobile+number
+        for i in range(mobile,endmobile):
+            r = threading.Thread(target=self.send,args=(self.ip,i))
+            r.start()
+    def send(self,ip,mobile):
+        time.sleep(1)
+        ip=str(ip)
+        port="6975"
+        mobile=str(mobile)
+        deviceid="1234567"
+        vnum="测SSR999"
+        link=tp.tcp_link(ip,port)
+        zcbody=tp.data_zc_body(deviceid,vnum)
+        zchead=tp.data_head(mobile,256,zcbody,1)
+        zcdata=tp.add_all((zchead+zcbody))
+        tp.send_data(link,zcdata)
+        zcres=tp.receive_data(link)
+        time.sleep(1)
+        jqbody=tp.data_jq_body(zcres)
+        jqhead=tp.data_head(mobile,258,jqbody,2)
+        jqdata=tp.add_all((jqhead+jqbody))
+        time.sleep(1)
+        tp.send_data(link,jqdata)
+        jqres=tp.receive_data(link)
+        time.sleep(1)
+        gpsbody=tp.data_gps_body(0,0,106.555555,29.000000,500,0,0,100)
+        gpsbody=gpsbody
+        gpshead=tp.data_head(mobile,512,gpsbody,3)
+        gpsdata=tp.add_all(gpshead+gpsbody)
+        tp.send_data(link,gpsdata)
+        res=tp.receive_data(link)
+        res=tp.dd(res)
+        x=0
+        while True:
+            y=0
+            while True:
+                y+=1
+                if y>=30:
+                    y=0
+                    heartbeat_body=""
+                    heartbeat_head=tp.data_head(mobile,2,heartbeat_body)
+                    heartbeat_data=tp.add_all(heartbeat_head)
+                    tp.send_data(link,heartbeat_data)
+                    tp.receive_data(link)
+                else:
+                    try:
+                        res=tp.receive_data(link)
+                        res=tp.dd(res)
+                        if res[3:8]=="80 01":
+                            pass
+                        else:
+                            reno=self.comboBox.currentIndex()
+                            if int(reno)==4:
+                                pass
+                            else:
+                                reno=tp.to_hex(reno,2)
+                                print reno
+                                id = res[2:6]
+                                ac = res[22:26]
+                                usual_body =ac+id+reno
+                                print usual_body
+                                usual_head = tp.data_head(mobile, 1, usual_body, 5)
+                                usual_redata = tp.add_all(usual_head + usual_body)
+                                tp.send_data(link, usual_redata)
+                                tp.receive_data(link)
+                    except:
+                        pass
+        tp.close(link)
+    def help(self):
+        reply = QtGui.QMessageBox.question(self, 'HELP', u'输入ID地址和手机号后，点击start，即可自动完成上线。\n'
+                                                         u'递增设置会根据设置数字，手机号自动递增上线。\n'
+                                                         u'应答设置会实时生效。\n'
+                                                         u'P.S.可以安装pyqt修改ui文件，如果你有更好的idea。',
+                                           )
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    F=QtGui.QWidget()
+    main=fwindow()
+    sys.exit(app.exec_())
