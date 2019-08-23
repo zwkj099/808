8e00d5c082125f456ac2fcd55dac4bf890698bf0
艾丽丽 <ailili@zwlbs.com>
2019-08-23 10:30:39 +0800

    注释

--- comman/Register_authentication_Heartbeat.py
+++ comman/Register_authentication_Heartbeat.py
@@ -10,7 +10,7 @@ def initial(tp,link,deviceid,vnum,mobile,version):
     data=tp.register(deviceid,vnum,mobile,version )
     tp.send_data(link,data)
     time.sleep(1)
-    Acode=tp.receive_data(link)
+    Acode=tp.receive_data(link) #0：成功；1：车辆已被注册；2：数据库中无该车辆；3：终端已被注册；4：数据库中无该终端
     #Acode = "7E 81 00 00 13 01 99 66 66 00 01 00 02 00 01 00 37 66 62 34 64 66 39 62 62 38 64 62 34 31 32 35 08 7E" #直接使用平台返回的鉴权应答
     
     #鉴权
