fb94f16242824cdbe169b06c72903b8cbfbdf4f9
艾丽丽 <ailili@zwlbs.com>
2019-12-04 17:59:10 +0800

    增加变量

--- comman/upload_location.py
+++ comman/upload_location.py
@@ -7,10 +7,10 @@ Created on 2019年8月19日
 import time
 def location(tp,link,mobile,pdict,ex808dict,sensordict,info,extrainfo_id,idlist,wsid,answer_number=0000):
     
-    zds,extrainfos,oils,wds,sds,yhs,zfs,zzs,gss,lcs,base,wifi,lys = info
+    zds,extrainfos,oils,wds,sds,yhs,zfs,zzs,gss,lcs,base,wifi,zdjc,dljc,lys = info
     gpsdata = tp.position(mobile, pdict['messageid'], 6, 1, pdict['alarm'], pdict['status'], pdict['jin'], pdict['wei'],\
                           pdict['high'], pdict['speed'], pdict['ti'], pdict['direction'], \
-                          tp.extra_info(extrainfo_id,extrainfos),tp.zd_body(wsid,zds),tp.f3_attach(idlist,oils,wds,sds,yhs,zfs,zzs,gss,lcs,base,wifi,lys), pdict['version'],answer_number)
+                          tp.extra_info(extrainfo_id,extrainfos),tp.zd_body(wsid,zds),tp.f3_attach(idlist,oils,wds,sds,yhs,zfs,zzs,gss,lcs,base,wifi,zdjc,dljc,lys), pdict['version'],answer_number)
     gpshead = tp.data_head(mobile, pdict['messageid'], gpsdata, 3, pdict['version'])
     gpsdata = tp.add_all(gpshead + gpsdata)
     tp.send_data(link, gpsdata)
