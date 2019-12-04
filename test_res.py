9b354094b274d66677c2772b5721d1037da84be6
艾丽丽 <ailili@zwlbs.com>
2019-12-04 21:10:19 +0800

    去掉数据组装

--- test_res.py
+++ test_res.py
@@ -35,11 +35,7 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
     mobile = str(mobile)
     deviceid = str(deviceid)
 
-    pdict, sichuandict, ex808dict, sensordict, bluetoothdict = readcig.readtestfile()
-    # 组装数据
-    zds, extrainfos, oils, wds, sds, yhs, zfs, zzs, gss, lcs,base,wifi, zdjc,dljc,lys = readcig.build_data(pdict, sichuandict, ex808dict,sensordict, bluetoothdict,deviceid)
-
-    info = [zds, extrainfos, oils, wds, sds, yhs, zfs, zzs, gss, lcs,base,wifi, zdjc,dljc,lys]
+    pdict, sichuandict, ex808dict, sensordict, bluetoothdict,info,zds, extrainfos = readcig.readtestfile(deviceid)#读取testconfig.xml变更，并组装成需要的列表变量
 
     """
     需要上传的附加信息或基于0200的扩展信息；十进制数，0或不填写表示不上传对应附加信息
@@ -50,7 +46,7 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
     extrainfo_id = [1,48,49]  # [1,2,3,20,21,22,23,24,48,49]#传入需要组装的附件信息ID,不传表示无附加信息;1：里程，2：油量，3：速度，48：信号强度，49：卫星颗数，20：视频相关报警，21：视频信号丢失报警状态，22：视频信号遮挡报警状态，23：存储器故障报警状态，24：异常驾驶行为报警详细描述
 
    #上传wifi数据时，必须同时上传基站数据，上传基站数据，0200状态要为未定位
-    idlist = [8,9,80]  # [34, 39, 65,69,79,80,81,83,112,128],传入需要组装的传感器ID，十进制数；33,34,35,36,37:温度；38,39,40,41:湿度；65,66,67,68:油量、液位；69,70:油耗；81:正反转；83:里程；84:蓝牙信标；112,113:载重；128,129:工时；8：基站数据；8、9：wifi数据
+    idlist = [8,9,79,80]  # [34, 39, 65,69,79,80,81,83,112,128],传入需要组装的传感器ID，十进制数；33,34,35,36,37:温度；38,39,40,41:湿度；65,66,67,68:油量、液位；69,70:油耗；79:电量检测,80:终端检测；81:正反转；83:里程；84:蓝牙信标；112,113:载重；128,129:工时；8：基站数据；8、9：wifi数据
     wsid = [0]  # 上传的主动安全报警类型，（冀标只有100和101）；0: 表示不带主动安全数据；100：驾驶辅助功能报警信息；101：驾驶员行为监测功能报警信息；112：激烈驾驶报警信息；102：轮胎状态监测报警信息；103：盲区监测报警信息；113：卫星定位系统报警信息；川冀标切换只需改端口；
 
     link = tp.tcp_link(ip, port)
@@ -114,7 +110,6 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
                         wsid[0] = 112
                     elif(wsid[0]==112):
                         wsid[0] = 100
-                info = readcig.build_data(pdict, sichuandict, ex808dict, sensordict, bluetoothdict, deviceid)
                 upload_location.location(tp, link, mobile, pdict, ex808dict, sensordict, info, extrainfo_id, idlist,wsid)
                 res = tp.receive_data(link)
                 res = tp.dd(res)
