1a481479371ddc48b77208cfcc7268e2d9c8b67d
yangdanyuan <820493977@mail.com>
2020-03-18 12:52:49 +0800

    上传位置前：增加获取redis最新位置的里程

--- db_operation/getdb_value.py
+++ db_operation/getdb_value.py
@@ -12,4 +12,18 @@ def getredis(key,host,db,pwd):
         return None
     return result
 
+def get_0partition_keys(host,db,pwd):
+    rdb = RedisConnect(host, 0, pwd)
+    keys = rdb.getkeys()
+    print keys
+    id = []
+    for key in keys:
+        if key.find("-vehiclemsgsn") != -1:
+            id.append((key.replace("-vehiclemsgsn", "")))
+
+    print id
+    return id
+
+
+
 
