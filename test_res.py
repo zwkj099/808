d9d4f0ba2ea2cf7146ae41084e6f488a93805bfb
艾丽丽 <ailili@zwlbs.com>
2020-05-21 18:02:25 +0800

    增加部分配置

--- test_res.py
+++ test_res.py
@@ -45,11 +45,11 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
     2.idlist：外设及传感器附加信息
     3.wsid：主动安全报警附加信息
     """
-    extrainfo_id = [1,2,3,20,21,22,23,24,48,49]#[1,48,49]  # [1,2,3,20,21,22,23,24,48,49]#传入需要组装的附件信息ID,不传表示无附加信息;1：里程，2：油量，3：速度，48：信号强度，49：卫星颗数，20：视频相关报警，21：视频信号丢失报警状态，22：视频信号遮挡报警状态，23：存储器故障报警状态，24：异常驾驶行为报警详细描述
+    extrainfo_id = [1]#[1,48,49]  # [1,2,3,20,21,22,23,24,48,49]#传入需要组装的附件信息ID,不传表示无附加信息;1：里程，2：油量，3：速度，48：信号强度，49：卫星颗数，20：视频相关报警，21：视频信号丢失报警状态，22：视频信号遮挡报警状态，23：存储器故障报警状态，24：异常驾驶行为报警详细描述
 
    #上传wifi数据时，必须同时上传基站数据，上传基站数据，0200状态要为未定位
-    idlist = [8,9,33,34,35,36,37,38,39,40,41,65,66,67,68,69,70,79,80,81,83,84,112,128]  # [34, 39, 65,69,79,80,81,83,112,128],传入需要组装的传感器ID，十进制数；33,34,35,36,37:温度；38,39,40,41:湿度；65,66,67,68:油量、液位；69,70:油耗；79:电量检测,80:终端检测；81:正反转；83:里程；84:蓝牙信标；112,113:载重；128,129:工时；8：基站数据；8、9：wifi数据
-    wsid = [100,101,112,102,103,113]  # 上传的主动安全报警类型，（冀标只有100和101）；0: 表示不带主动安全数据；100：驾驶辅助功能报警信息；101：驾驶员行为监测功能报警信息；112：激烈驾驶报警信息；102：轮胎状态监测报警信息；103：盲区监测报警信息；113：卫星定位系统报警信息；川冀标切换只需改端口；
+    idlist = [8]  # [34, 39, 65,69,79,80,81,83,112,128],传入需要组装的传感器ID，十进制数；33,34,35,36,37:温度；38,39,40,41:湿度；65,66,67,68:油量、液位；69,70:油耗；79:电量检测,80:终端检测；81:正反转；83:里程；84:蓝牙信标；112,113:载重；128,129:工时；8：基站数据；8、9：wifi数据
+    wsid = [231]  # 上传的主动安全报警类型；225-231是中位主动安全数据，冀标只有100和101；0: 表示不带主动安全数据；100：驾驶辅助功能报警信息；101：驾驶员行为监测功能报警信息；112：激烈驾驶报警信息；102：轮胎状态监测报警信息；103：盲区监测报警信息；113：卫星定位系统报警信息；川冀标切换只需改端口；
 
     link = tp.tcp_link(ip, port)
     # 注册、鉴权、心跳
@@ -70,8 +70,8 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
 
 
     #设置redis里程，传入车辆id，公共参数ex808dict
-    vehicle_id = getmysql_vehicleid.get_vehicleid('192.168.24.142'  ,'root' ,'Zwkj@123Mysql' ,'clbs',vnum)
-    print vehicle_id
+    #vehicle_id = getmysql_vehicleid.get_vehicleid('192.168.24.142'  ,'root' ,'Zwkj@123Mysql' ,'clbs',vnum)
+    # print vehicle_id
     # setMileage.setto_redismel(vehicle_id,ex808dict,pdict['redishost'],pdict['db'],pdict['pwd'])
     # print "redis mel: "+str(ex808dict['mel'])
 
@@ -81,16 +81,16 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
     #数据库操作    # dbop = db_operation.interface_db.interface_db()
     # print dbop.mysqldata('select * from paas_monitorInfo')
 
-    auto = 0;  # 是否要跑自动化脚本？
+    auto = 0;  # 为0不跑自动化脚本，为１要跑自动化脚本
     aa=1
-    if auto == 1:  # 是否要跑自动化脚本？
+    if auto == 1:  # auto == 1，则要跑自动化脚本
         auto_test(tp, link, mobile, vnum)
-    elif (pdict['ti'] != 0):  # 补传数据
+    elif (pdict['ti'] != 0):  # 是否要补传数据；pdict['ti'] != 0补传，pdict['ti'] = 0不补传
         wsid1 = []
         wsid1,excel_list_k=buchuan1.deal_data(pdict, sichuandict, ex808dict, sensordict,bluetoothdict,wsid1,i)#读取excel表格数据，改变速度、初始里程、报警事件
         buchuan1.upload(tp, link, mobile, pdict, sichuandict, ex808dict, sensordict,bluetoothdict, extrainfo_id, idlist, wsid1,excel_list_k,deviceid,port,tal)
 
-    elif aa==1:  #数据准备，也要根据情况设置extrainfo_id,idlist ,wsid，该绑定传感器的要到平台绑定传感器
+    elif aa==0:  #数据准备，也要根据情况设置extrainfo_id,idlist ,wsid，该绑定传感器的要到平台绑定传感器
         print "数据准备开始"
         dataready_test.datatest(tp, link, mobile, extrainfo_id, idlist, wsid,deviceid,port,vehicle_id)
         print "准备数据完成"
@@ -127,6 +127,15 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
                         wsid[0] = 112
                     elif(wsid[0]==112):
                         wsid[0] = 100
+                    #如果是中位标准，可以先注释以上６行代码，开启下面的判断
+                    # if (wsid[0] == 225):
+                    #     wsid[0] = 226
+                    # elif (wsid[0] == 226):
+                    #     wsid[0] = 227
+                    # elif (wsid[0] == 227):
+                    #     wsid[0] = 228
+                    # elif (wsid[0]) == 228:
+                    #     wsid[0] == 225
                 upload_location.location(tp, link, mobile, pdict, sichuandict, ex808dict, sensordict, bluetoothdict,
                                          extrainfo_id, idlist, wsid, deviceid, port)
                 res = tp.receive_data(link)
@@ -144,7 +153,6 @@ def test1(ip, port, mobile, deviceid, vnum, name, qualification,i=0,tal=0):
                         answer_number = res[32:36]
                     reno = "00"
                     if id in ["8201", "8202","8802","8500"]:
-                        # reply_position.reply_pos(tp, link, mobile, pdict, ex808dict, sensordict, info, extrainfo_id,idlist, wsid, answer_number, res, id, reno)
                         reply_position.reply_pos(tp, link, mobile, pdict,sichuandict, ex808dict, sensordict,bluetoothdict, extrainfo_id,
                                                  idlist, wsid,  deviceid, port,answer_number, res, id, reno)
 
@@ -187,12 +195,12 @@ def ano_res(res):
     return re_list
 
 # 设置接入ip
-ip = "192.168.24.142"  # 218.78.40.57,"111.41.48.133"#"192.168.24.142"
+ip = "192.168.24.143"  # 218.78.40.57,"111.41.48.133"#"192.168.24.142"
 # ip="zoomwell.cn"
-port = 6994  # 6994川标,6995冀标，6975部标,6996桂标，6997苏标，6998浙标，6999吉标，7000陕标,7002沪标
-deviceid ='B000027' #20190928
-mobile =13200000027 #123456789
-vnum = u"渝B0012"  #桂BB001
+port = 7003  # 6994川标,6995冀标，6975部标,6996桂标，6997苏标，6998浙标，6999吉标，7000陕标,7002沪标；7003中位标准
+deviceid ='1545125' #20190928
+mobile =15897874541 #123456789
+vnum = u"测JTS882"  #桂BB001
 cont = 0
 name = "驾驶员桂A0002"
 qualification = 14303529463400355011
@@ -202,7 +210,7 @@ thread_list = []
 for i in range(0, 1):
     t = threading.Thread(target=test1, args=(ip, port, mobile, deviceid, vnum, name, qualification,i,tal))
     t.start()
-    deviceid += 1
+#    deviceid += 1
     mobile += 1
     qualification += 1
     cont += 1
