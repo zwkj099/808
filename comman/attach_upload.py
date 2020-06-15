a3f6e8e40038d8ae461535d2ec739b1ac1104b85
蒋添诗 <404700138@qq.com>
2020-06-15 10:43:20 +0800

    优化附件上传

--- comman/attach_upload.py
+++ comman/attach_upload.py
@@ -17,14 +17,14 @@ def attach_upload(tp,res,mobile,version):
         while len(mobile) < 20:
             mobile = '0' + mobile
         iplenth = (int(res[36:38],16))*2 #获取9208指令中服务器地址长度字段
-        ip2 = "112.126.64.32"
-        # ip2 = "192.168.24.142"
+        # ip2 = "112.126.64.32"
+        ip2 = "192.168.24.142"
         port2 = 7901
         alarmflag = res[42+iplenth:84+iplenth]  # 获取报警标识号
         alarm_number = res[84+iplenth:148+iplenth]
         # 文件名（00_e1_e100_0_b87441129fb34ef8969fd0de2739d15e.jpg）转成16进制格式
         #文件名称可修改，文件大小不要大于64kb
-        filepath = 'C:\\Users\\zwkj\\Desktop\\00_E1_E100_0_b87441129fb34ef8969fd0de2739d15e.jpg'
+        filepath = 'C:\\Users\\zwkj\\Desktop\\00_E1_E100_0_b87441129fb34ef8969fd0de2739d1330.jpg'
         #filename1为原始文件名，filename转为16进制后的名称
         filename1 = os.path.basename(filepath)
         filename = tp.character_string(filename1)
@@ -33,7 +33,6 @@ def attach_upload(tp,res,mobile,version):
         filesize = os.stat(filepath).st_size
         print filesize
         filesize1 = tp.to_hex(filesize,8)
-        # filename = '30305F65315F653130305F305F62383734343131323966623334656638393639666430646532373339643135652E6A7067'
 
         filenamelen = str(hex(len(filename) / 2))[2:]  # 文件名长度
 
@@ -45,7 +44,6 @@ def attach_upload(tp,res,mobile,version):
         link1 = tp.tcp_link(ip2, port2)
         tp.send_data(link1,redata)
         tp.receive_data(link1)
-        # time.sleep(30)
         # 1211指令的消息体跟附件信息内容一样
         fileinfo_head = tp.data_head(mobile, 4625, attachlist, 5, version)
         fileinfo = tp.add_all(fileinfo_head + attachlist)
@@ -57,14 +55,12 @@ def attach_upload(tp,res,mobile,version):
         m = filesize // 65536
         n = filesize % 65536
         picdata = open(filepath, 'rb')
-        # streamhead = bytearray(b'\x30\x31\x63\x64')
         streamhead = '30316364'
         if m == 0:
             picbytes = picdata.read(filesize)
             data1 = streamhead + filename2 + '00000000' + filesize1
             data2 = tp.to_pack(data1)
             data = data2 + picbytes
-            print data
             link1.send(data)
         else:
             for i in range(m+1):
@@ -93,13 +89,6 @@ def attach_upload(tp,res,mobile,version):
         flishdata = tp .add_all(head + usual_body)
         tp.send_data(link1,flishdata)
         tp.receive_data(link1)
-
-        #文件上传完成指令应答9212
-        # usual_body = filenamelen + filename +'00' +'00' + '00' + '00000000' +'00000000'
-        # head = tp.data_head(mobile, 37394, usual_body, 5, version)
-        # rdata = tp.add_all(head + usual_body)
-        # tp.send_data(link1,rdata)
-        # tp.receive_data(link1)
 
 
         tp.close(link1)
